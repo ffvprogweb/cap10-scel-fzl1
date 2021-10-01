94327add9b8663fdf30b5dc20a429e04ed9e2dd4
Edson Saraiva de Almeida <68782201+ffvprogweb@users.noreply.github.com>
2021-10-01 17:01:46 -0300

    cap10 - inclusao do caso de teste aluno ja cadastrado

--- scel/src/test/java/testeDeSistema/REQ02MantemAlunoTests.java
+++ scel/src/test/java/testeDeSistema/REQ02MantemAlunoTests.java
@@ -77,7 +77,7 @@ public class REQ02MantemAlunoTests {
 		driver.findElement(By.id("cep")).sendKeys("04280130");
 		driver.findElement(By.cssSelector(".btn:nth-child(1)")).click();
 		espera();
-		//depuracao
+		// depuracao
 		assertEquals("Rua Frei João", driver.findElement(By.cssSelector("td:nth-child(6)")).getText());
 		// **********************************************************************************
 		// quando o usuario altera o CEP do endereco
@@ -93,6 +93,71 @@ public class REQ02MantemAlunoTests {
 		// teardown - exclusao do registro
 		// ***********************************************************************************
 		driver.findElement(By.linkText("Excluir")).click();
+	}
+
+	@Test
+	public void ct03cadastraraluno_ja_cadastrado() {
+		// ******************************************************************************
+		// dado que o aluno esta cadastrado
+		// ******************************************************************************
+		driver.findElement(By.name("username")).click();
+		driver.findElement(By.name("username")).sendKeys("jose");
+		driver.findElement(By.name("password")).sendKeys("123");
+		driver.findElement(By.cssSelector("button")).click();
+		driver.findElement(By.linkText("Alunos")).click();
+		espera();
+		driver.findElement(By.id("ra")).click();
+		driver.findElement(By.id("ra")).sendKeys("1111");
+		driver.findElement(By.id("nome")).click();
+		driver.findElement(By.id("nome")).sendKeys("jose");
+		driver.findElement(By.id("email")).sendKeys("jose@gmail.com");
+		driver.findElement(By.id("cep")).sendKeys("04280130");
+		driver.findElement(By.cssSelector(".btn:nth-child(1)")).click();
+		// asserts com objetivo de dupuracao
+		assertEquals("Rua Frei João", driver.findElement(By.cssSelector("td:nth-child(6)")).getText());
+		assertEquals(("Lista de alunos"), driver.findElement(By.id("titulopagina")).getText());
+		assertEquals("https://ts-scel-web.herokuapp.com/sig/alunos", driver.getCurrentUrl());
+		assertTrue(driver.getPageSource().contains("1111"));
+		// ******************************************************************************
+		// quando tenta cadastrar um aluno ja cadastrado
+		// ******************************************************************************
+		driver.findElement(By.linkText("Voltar")).click();
+		driver.findElement(By.linkText("Alunos")).click();
+		espera();
+		driver.findElement(By.id("ra")).click();
+		driver.findElement(By.id("ra")).sendKeys("1111");
+		driver.findElement(By.id("nome")).sendKeys("jose");
+		driver.findElement(By.id("email")).sendKeys("jose@gmail.com");
+		driver.findElement(By.id("cep")).sendKeys("04280130");
+		driver.findElement(By.cssSelector(".btn:nth-child(1)")).click();
+		// *******************************************************************************
+		// entado retorna dados invalidos
+		// *******************************************************************************
+		assertEquals("Dados invalidos", driver.findElement(By.cssSelector(".text-danger")).getText());
+		// ******************************************************************************
+		// teardown - exclusao do registro
+		// ******************************************************************************
+		vars.put("window_handles", driver.getWindowHandles());
+		driver.findElement(By.cssSelector(".btn:nth-child(2)")).click(); // botao consultar
+		vars.put("win7071", waitForWindow(2000));
+		driver.switchTo().window(vars.get("win7071").toString());
+		driver.findElement(By.cssSelector(".delete")).click();
+	}
+
+	
+
+	public String waitForWindow(int timeout) {
+		try {
+			Thread.sleep(timeout);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Set<String> whNow = driver.getWindowHandles();
+		Set<String> whThen = (Set<String>) vars.get("window_handles");
+		if (whNow.size() > whThen.size()) {
+			whNow.removeAll(whThen);
+		}
+		return whNow.iterator().next();
 	}
 
 	public void espera() {
